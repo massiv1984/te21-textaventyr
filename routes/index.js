cbd19f15f9fbb80dd6fcea7c5035a18c8ee1a627
Jens Andreasson <jens.andreasson@ntig.se>
2024-01-11 16:15:26 +0100

    ✨ starting example

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+const story = require('../data/story.json')
+
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Välkommen' })
+  console.log(story.parts[0])
+  res.render('index.njk', { title: 'Welcome', part: story.parts[0] })
+})
+
+router.get('/story/:id', function (req, res) {
+  const part = story.parts.find((part) => part.id === parseInt(req.params.id))
+  if (!part) {
+    res.status(404).render('404.njk', { title: '404' })
+    return
+  }
+  res.render('part.njk', { title: part.name, part: part })
 })
 
 module.exports = router
