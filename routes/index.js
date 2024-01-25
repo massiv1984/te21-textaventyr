966b26e70fa7c35998dccc31fcb310be6fb09a4f
massiv1984 <emil.kero@elev.ga.ntig.se>
2024-01-25 11:18:42 +0100

    session

--- routes/index.js
+++ routes/index.js
@@ -28,8 +28,14 @@ router.get('/dbtest/:id', async (req, res) => {
       .query(`SELECT * FROM emil_part WHERE id = ${id}`)
     const [options] = await pool
       .promise()
-      .query(`SELECT * FROM jens_option WHERE part_id = ${id}`)
-    res.json({ parts, options })
+      .query(`SELECT * FROM emil_option WHERE part_id = ${id}`)
+    //res.json({ parts, options })
+    const part = { ...parts[0], options}
+    res.render('part.njk', {
+      username: req.session.username,
+      title: part.name,
+      part,
+    })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
